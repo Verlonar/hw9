8d10f588786d335cffd4daeadea08817dedfa608
Verlonar <garvlok@gmail.com>
2022-04-01 16:50:18 +0300

    first high lvl try

--- src/ru/verlonar/Main.java
+++ src/ru/verlonar/Main.java
@@ -4,17 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Library library = new Library(10);
+
         Author author1 = new Author("Vanya", "Ivanov");
         Author author2 = new Author("Petya", "Petrov");
 
         Book book1 = new Book("First book", author1, 2002);
         Book book2 = new Book("Second book", author2, 2014);
 
-        System.out.println(book1);
+        library.addBook(book1);
+        library.addBook(book2);
 
-        book1.setPublicationYear(2010);
+        library.printBooksInfo();
 
-        System.out.println(book1);
-        System.out.println(book2);
+        library.printBookInfoByName("First book");
+
+        library.changeBookPublicationYearByName("Second book", 2022);
+        library.printBookInfoByName("Second book");
+        library.printBookInfoByName("Non-existent book");
     }
 }
